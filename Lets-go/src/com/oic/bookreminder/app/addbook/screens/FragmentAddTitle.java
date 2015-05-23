5c0406b8c8960ebe2c8bc0787075991663e7ef97
Pham Khac <khacpv@gmail.com>
2015-05-23 09:12:13 +0700

    add Animations libs

--- Lets-go/src/com/oic/bookreminder/app/addbook/screens/FragmentAddTitle.java
+++ Lets-go/src/com/oic/bookreminder/app/addbook/screens/FragmentAddTitle.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.BounceInterpolator;
+import android.widget.TextView;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.oic.bookreminder.R;
 import com.oic.bookreminder.app.AppFragment;
 
@@ -11,6 +15,7 @@ import com.oic.bookreminder.app.AppFragment;
  * Created by khacpham on 5/21/15.
  */
 public class FragmentAddTitle extends AppFragment{
+    TextView title;
     @Override
     protected View inflateLayout(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_addtitle,container,false);
@@ -18,7 +23,7 @@ public class FragmentAddTitle extends AppFragment{
 
     @Override
     protected void findControls(View viewParent) {
-
+        title = (TextView)viewParent.findViewById(R.id.title);
     }
 
     @Override
@@ -28,6 +33,6 @@ public class FragmentAddTitle extends AppFragment{
 
     @Override
     protected void initializeDefaultData() {
-
+        YoYo.with(Techniques.Tada).duration(1500).delay(500).interpolate(new BounceInterpolator(getActivity(),null)).playOn(title);
     }
 }
